4b7ea1f4ab18e65ebcb7b9b40be73310828a5e6f
radi <radi@comsoft.bg>
2020-06-01 15:19:07 +0300

    fixed login

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,21 @@ function setUserType(ut) {
     userType = ut;
 }
 
+
+function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    if (username === "admin" && password === "admin") {
+        setUserType(0);
+        window.location = "current-courses.html";
+    } else if (username === "pesho" && password === "1234") {
+        setUserType(1);
+        window.location = "course-dashboard.html";
+    } else {
+        document.getElementById("invalidCredentials").style.display = "inline";
+    }
+}
+
 function myCourses(){
     if(userType === 0){
         window.location = 'current-courses.html';
